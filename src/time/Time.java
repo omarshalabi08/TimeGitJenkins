443122cc69f394a47283950c2c8580000ca221a6
shalabom <shalabom@sheridancollege.ca>
2023-03-13 08:48:56 -0400

    Commit

--- src/time/Time.java
+++ src/time/Time.java
@@ -25,7 +25,7 @@ public class Time
 	{
 		
 		int totalSeconds = getTotalSeconds("10:10:10");
-	System.out.println("Total Seconds  "+ totalSeconds);
+	System.out.println("Github Total Seconds  "+ totalSeconds);
 	
 
 		try
